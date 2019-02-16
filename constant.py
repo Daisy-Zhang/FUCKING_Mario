be39aaea93ebd22830aae5c00cdfd3082b64b3e1
张岱墀 <zhang_daichi@163.com>
2019-02-16 23:03:07 +0800

    add constant file

---
+++ constant.py
@@ -0,0 +1,5 @@
+level1_board1_start = (0, 350)
+level1_board1_end = (500, 350)
+
+level1_board2_start = (500, 350)
+level1_board2_end = (500, 480)
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import pygame
-import time
+import constant
 from pygame.locals import *
 from sys import exit
  
 pygame.init()
+level = 1
 screen = pygame.display.set_mode((640, 480), 0, 32)
 
 player_image_file = 'image/player.png'
- 
 player = pygame.image.load(player_image_file).convert()
-
 player_x = 0
 player_y = 300
 
 jump_flag = 0
 
 while True:
+
     if jump_flag == 1:
         player_y += 50
         jump_flag = 0
@@ -38,8 +38,8 @@ while True:
 
     screen.fill((0,0,0))
     board_col = (255, 255, 255)
-    pygame.draw.line(screen, board_col, (0, 350), (500, 350), 3)
-    pygame.draw.line(screen, board_col, (500, 350), (500, 480), 3)
+    pygame.draw.line(screen, board_col, constant.level1_board1_start, constant.level1_board1_end, 3)
+    pygame.draw.line(screen, board_col, constant.level1_board2_start, constant.level1_board2_end, 3)
     screen.blit(player, (player_x, player_y))            
     
     pygame.display.update()
