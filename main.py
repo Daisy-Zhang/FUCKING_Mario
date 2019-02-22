027a23f49676a06657c474e0a50e6e92fc6063e5
schuangs <47692627+schuangs@users.noreply.github.com>
2019-02-23 01:32:24 +0800

    big change

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ black = (0, 0, 0)
 blue = (0, 0, 255)
 light_blue = (128, 128, 255)
 
-deadline = 700		# the falling deadline
+enemies = [[],[],[]]    #   collection of enemies of different level
+
+destination = 4     #  destination level
 
 
 def toNextLevel(player_x, player_y):
@@ -48,15 +50,15 @@ def showMap(level):
         box_col = box_[2]
         pygame.draw.rect(screen, box_col, (x, y, length, height))
 
-    # load enemy test
-    
-    # enemy1 = pygame.image.load(role.level1_enemy1.getEnemyImageFile()).convert()
-    # if role.level1_enemy1.judgeDeath() == constant.ROLE_ALIVE :
-    #     role.level1_enemy1.enemyMove()
-    #     screen.blit(enemy1, (role.level1_enemy1.getEnemyX(), role.level1_enemy1.getEnemyY()))
-    # screen.blit(player, (player_x, player_y))
+def touch(player, enemy):
+    touched = True
+    if player.getRoleX() > enemy.getRoleX() + enemy.getRoleXSize() or player.getRoleX() + player.getRoleXSize() < enemy.getRoleX():
+        touched = False
+    if player.getRoleY() > enemy.getRoleY() + enemy.getRoleYSize() or player.getRoleY() + player.getRoleYSize() < enemy.getRoleY():
+        touched = False
+    return touched
 
-
+#  add player1 in this game
 player1 = role.role()
 player1.setRoleX(player_x)
 player1.setRoleY(player_y)
@@ -64,24 +66,76 @@ player1.setRoleXSize(player_x_size)
 player1.setRoleYSize(player_y_size)
 player1.setImageFile('image/player.png')
 
+#  level 1 no enemy
+
+#  add enemy1 in level 2
+enemy2 = role.role()
+enemy2.setHealth(100)
+enemy2.setRoleType(1)
+enemy2.setRoleXSize(56)
+enemy2.setRoleYSize(56)
+enemy2.setRoleX(540)
+enemy2.setRoleY(350 - 56)
+enemy2.setImageFile('image/enemy1.png')
+enemies[1].append(enemy2)
+
+
+#   add enemy3 in level 3
+enemy3 = role.role()
+enemy3.setHealth(100)
+enemy3.setRoleType(1)
+enemy3.setRoleXSize(56)
+enemy3.setRoleYSize(56)
+enemy3.setRoleX(400)
+enemy3.setRoleY(100)
+enemy3.setStartMoving(250)
+enemy3.setImageFile('image/enemy1.png')
+enemies[2].append(enemy3)
+
+#    add enemy4 in level3
+enemy4 = role.role()
+enemy4.setHealth(100)
+enemy4.setRoleType(1)
+enemy4.setRoleXSize(54)
+enemy4.setRoleYSize(54)
+enemy4.setRoleX(620)
+enemy4.setRoleY(200)
+enemy4.setStartMoving(400)
+enemy4.setImageFile('image/enemy2.png')
+enemies[2].append(enemy4)
+
 while True:
 
     if toNextLevel(player1.getRoleX(), player1.getRoleY()):
         player1.setRoleX(0)
         level += 1
+        if level == destination:
+            print('Your Win!')
+            exit()
 
     ground, left, right = move.getGround(player_x, player_y, player_x_size, player_y_size, level)    # the ground position y coordinate
 
-    move.move(player1, level)
+    move.move(player1, level)  
+
   
-    if player1.getRoleY() > deadline:   # determine whether player is falling to death
+    if player1.getRoleY() > move.deadline:   # determine whether player is falling to death
     	print('Game Over')
     	exit()
     
-    
+
+    showMap(level)
+
+    for enemy in enemies[level - 1]:
+        if player1.getRoleX() > enemy.getStartMoving():
+            enemy.setApp(1)
+        if not enemy.getHealth() == 0 and enemy.getApp() == 1:
+            move.move(enemy, level)
+            enemyImage = pygame.image.load(enemy2.getRoleImageFile()).convert()
+            screen.blit(enemyImage, (enemy.getRoleX(), enemy.getRoleY()))
+        if touch(player1, enemy):
+            print('Game Over')
+            exit()
 
     player = pygame.image.load(player1.getRoleImageFile()).convert()
-    showMap(level)
     screen.blit(player,(player1.getRoleX(), player1.getRoleY()))
-
     pygame.display.update()
