648cae8746240de663d12989229f30bdd4641607
schuangs <47692627+schuangs@users.noreply.github.com>
2019-02-22 20:02:17 +0800

    Add files via upload

---
+++ role.py
@@ -0,0 +1,144 @@
+import constant
+import move
+
+class role():            # basic class, funny just for test
+    
+    role_type = 0    #  role_type = 0 player,   role_type = 1 enemy
+    role_x = 0    #  position of the role
+    role_y = 0
+    role_image_file = "null"    #   role image file name
+
+    role_x_size = 0    #  role size
+    role_y_size = 0
+
+    health = 0    #   health of the role
+    top_health = 100
+
+    total_jump_time = 200   # total time of a jump on a ground
+    jump_height = 120    # height of a jump on a ground
+
+    move_speed = 0.1      # horizental move speed
+    top_move_speed = 0.6    # horizontal top move speed
+
+    up_speed = 0    # the speed of up going
+    jump_direction = 0  # 0 jump vertically    1 jump towards left    2 jump towards right
+   
+    or_acceleration = 0.0003    # original horizental move acceleration
+    acceleration = or_acceleration    # horizental move acceleration
+
+
+    def setRoleType(self, rt):
+        self.role_type = rt
+
+    def setImageFile(self, s):
+        self.role_image_file = s
+
+    def setRoleX(self, x):
+        self.role_x = x
+    
+    def setRoleY(self, y):
+        self.role_y = y
+
+    def setRoleXSize(self, l):
+        self.role_x_size = l
+
+    def setRoleYSize(self, h):
+        self.role_y_size = h
+
+    def setHealth(self, heal):
+        self.health = heal
+
+    def setTotalJumpTime(self, totJT):
+        self.total_jump_time = totJT
+
+    def setJumpHeight(self, h):
+        self.jump_height = h
+
+    def setMoveSpeed(self, mv):
+        self.move_speed = mv
+
+    def setUpSpeed(self, us):
+        self.up_speed = us
+
+    def setJumpDirection(self, jumpDR):
+        self.jump_direction = jumpDR
+
+    def setTopMoveSpeed(self, topMS):
+        self.top_move_speed = topMS
+
+    def setOrAcceleration(self, orA):
+        self.or_acceleration = orA
+
+    def setAcceleration(self, acc):
+        self.acceleration = acc
+
+    def getRoleType(self):
+        return self.role_type
+
+    def getRoleImageFile(self):
+        return self.role_image_file
+
+    def getRoleX(self):
+        return self.role_x
+    
+    def getRoleY(self):
+        return self.role_y
+
+    def getRoleXSize(self):
+        return self.role_x_size
+
+    def getRoleYSize(self):
+        return self.role_y_size
+
+    def getHealth(self):
+        return self.health
+
+    def getTotalJumpTime(self):
+        return self.total_jump_time
+
+    def getJumpHeight(self):
+        return self.jump_height
+
+    def getMoveSpeed(self):
+        return self.move_speed
+
+    def getUpSpeed(self):
+        return self.up_speed
+
+    def getJumpDirection(self):
+        return self.jump_direction
+
+    def getTopMoveSpeed(self):
+        return self.top_move_speed
+
+    def getOrAcceleration(self):
+        return self.or_acceleration
+
+    def getAcceleration(self):
+        return self.acceleration
+
+    def getA(self):
+        return -8 * self.jump_height / (self.total_jump_time ** 2)   # coefficient of jump action
+
+    def getJumpSpeed(self):
+        return - self.getA() * self.total_jump_time / 2
+
+    def judgeDeath(self):
+        if self.health <= 0:
+            return constant.ROLE_DIED
+        else:
+            return constant.ROLE_ALIVE
+
+    #maybe next time to finish them
+    #def beAttacked(self):
+    #def beTouched(self, player_x, player_y):
+
+# just for quick test
+# level1_enemy1 = enemy()
+# level1_enemy1.setImageFile("image/enemy1.png")
+# level1_enemy1.setEnemyInitialPos(100, 290)
+# level1_enemy1.setEnemyTrack(100, 300, 200, 300)
+# level1_enemy1.setDirection('h')
+# level1_enemy1.setInitialFaceto('r')
+# level1_enemy1.setSpeed(2)
+# level1_enemy1.setHealth(1)
